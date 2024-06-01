b368c4d87f49e8c5ca180f60749dd302fc692498
otoap <Otaraptsiauri@gmail.com>
2024-06-01 19:57:31 +0400

    add math operation symbols in screen

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 "use strict"
+let screen = document.getElementsByClassName("calc-display")[0];
+const buttons = document.getElementsByTagName("button");
+let clicker = Array.from(buttons);
+
+let holder = 0;
+
+clicker.forEach((btn)=> {
+    btn.addEventListener("click", ()=> {
+        let newBtn = btn.textContent
+        if(screen.textContent == 0 && !isNaN(newBtn)) {
+            screen.innerHTML = newBtn;
+        }else if(!isNaN(newBtn) || !isNaN(screen.textContent[screen.textContent.length-1]) && newBtn != "=" && newBtn != "DEL" && newBtn != "RESET")
+            if(screen.textContent == 0) {
+                return
+            }else {
+            screen.innerHTML += newBtn
+            }
+    })
+})
+
+
+
